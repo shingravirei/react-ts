e8cea88accf37b08dd228dd5e53719b7cb38443b
shingravirei <zedfromionia@gmail.com>
2021-04-10 17:38:32 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    root: true,
+    env: {
+        es2021: true,
+        browser: true,
+        jest: true,
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    settings: {
+        react: {
+            pragma: 'React',
+            version: 'detect',
+        },
+    },
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    rules: {
+        'react-in-jsx-scope': off,
+    },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+        </div>
+    );
+};
+
+export default App;
