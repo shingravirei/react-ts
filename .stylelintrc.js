5bfa5ce07e76587c760bef6aedd784c563be5da9
shingravirei <42350757+shingravirei@users.noreply.github.com>
2021-08-24 14:53:40 -0300

    add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'stylelint-config-standard',
+    rules: {
+        'value-list-comma-newline-after': null,
+        indentation: [4],
+        'declaration-colon-newline-after': null,
+    },
+};
