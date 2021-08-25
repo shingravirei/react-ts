cc2f3d0276b9ae057af169e57eedcfe9ac23b3c4
shingravirei <42350757+shingravirei@users.noreply.github.com>
2021-08-25 10:40:14 -0300

    Revert "add stylelint"
    This reverts commit 5bfa5ce07e76587c760bef6aedd784c563be5da9.

--- .stylelintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: 'stylelint-config-standard',
-    rules: {
-        'value-list-comma-newline-after': null,
-        indentation: [4],
-        'declaration-colon-newline-after': null,
-    },
-};
