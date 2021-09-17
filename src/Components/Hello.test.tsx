ac7f0b87731bd46f735977680129257aeb76d30a
shingravirei <42350757+shingravirei@users.noreply.github.com>
2021-09-16 21:55:19 -0300

    bump deps

--- src/Components/Hello.test.tsx
+++ src/Components/Hello.test.tsx
@@ -1,6 +1,6 @@
+import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import Hello from './Hello';
-import '@testing-library/jest-dom/extend-expect';
 
 it('it works', () => {
     const { debug, getByText } = render(<Hello />);
