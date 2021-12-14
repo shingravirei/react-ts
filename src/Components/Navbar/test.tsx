28e2016a6d38fe45bcba5ba639a090a62c6d12bd
shingravirei <42350757+shingravirei@users.noreply.github.com>
2021-12-14 17:16:45 -0300

    update test

--- src/Components/Navbar/test.tsx
+++ src/Components/Navbar/test.tsx
@@ -1,15 +1,15 @@
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import Navbar from '.';
 
 it('Navbar', () => {
-    const { getByText } = render(
+    render(
         <MemoryRouter>
             <Navbar />
         </MemoryRouter>,
     );
 
-    expect(getByText(/Home/i)).toBeInTheDocument();
-    expect(getByText(/About/i)).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /Home/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /About/i })).toBeInTheDocument();
 });
