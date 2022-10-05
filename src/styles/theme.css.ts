df59cb8b176fbf9954342177ba6f2711b2fab63c
shingravirei <42350757+shingravirei@users.noreply.github.com>
2022-10-04 21:48:51 -0300

    refactor: delete unecessary stuff

--- src/styles/theme.css.ts
+++
@@ -1,8 +0,0 @@
-import { createGlobalTheme } from '@vanilla-extract/css';
-import { colors, font, space } from './tokens';
-
-export const vars = createGlobalTheme(':root', {
-	colors,
-	space,
-	font,
-});
