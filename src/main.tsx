4ea915670902a9574f9ddd555fae306d9facf775
shingravirei <42350757+shingravirei@users.noreply.github.com>
2022-12-09 17:25:22 -0300

    feat: add vanilla extract

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode, Suspense } from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import '~/index.css';
 import { router } from '~/router';
+import '~/styles/app.css';
 
 const container = document.querySelector('#root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,32 @@
+import { sprinkles } from '~/styles/sprinkles.css';
+
 const Home = () => (
 	<div>
 		<h1>Home</h1>
+		<button className={sprinkles({ py: 'xs', px: 'sm', fontSize: 'md' })}>
+			button
+		</button>
+		<div
+			className={sprinkles({
+				display: 'flex',
+				gap: ['md', 'lg', 'xl'],
+			})}
+		>
+			<div
+				style={{
+					width: '50px',
+					height: '50px',
+					backgroundColor: 'tomato',
+				}}
+			/>
+			<div
+				style={{
+					width: '50px',
+					height: '50px',
+					backgroundColor: 'tomato',
+				}}
+			/>
+		</div>
 	</div>
 );
 
